b7ab055bf4ce68a6413dd2611023f46a91e3dae8
Jorge Ricardo Leão <jotalion.4.jlo@gmail.com>
2021-05-16 21:09:41 -0300

    adicionando novas funcionalidades ao site e finalizando index

--- site/script.js
+++ site/script.js
@@ -4,3 +4,29 @@ function abrir_hamburguer() {
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change");
 }
+
+function enviar() {
+    nome = String(inp_nome.value);
+    email = String(inp_email.value);
+    telefone = Number(inp_telefone.value);
+    veiculos = Number(combo_veiculos.value);
+
+    email.indexOf('@') == -1 || email.indexOf('.com') == -1 ? 
+        alert('Email invalido') 
+        : alert('Email cadastrado com sucesso');
+
+    veiculos != 1 && veiculos != 2 && veiculos != 3 ?
+        alert('Veiculo ainda não foi selecionado')
+        : 0;
+}
+
+function analisar() {
+    nome = String(inp_nome.value);
+    email = String(inp_email.value);
+    telefone = Number(inp_telefone.value);
+    veiculos = Number(combo_veiculos.value);
+
+    email.indexOf('@') == -1 || email.indexOf('.com') == -1 ?
+        inp_email.style.border = `1px solid red`
+        : inp_email.style.border = `1px solid green`;
+}
