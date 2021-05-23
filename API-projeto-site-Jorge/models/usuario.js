dbfd08d1242ca54bece458ccf92fd5c7325539d0
Jorge Ricardo Leão <jotalion.4.jlo@gmail.com>
2021-05-22 23:25:21 -0300

    subindo tabela leads para nuvem e alterando pagina de login

---
+++ API-projeto-site-Jorge/models/usuario.js
@@ -0,0 +1,50 @@
+	'use strict';
+
+/* 
+lista e explicação dos Datatypes:
+https://codewithhugo.com/sequelize-data-types-a-practical-guide/
+*/
+
+module.exports = (sequelize, DataTypes) => {
+    let cliente_Jorge = sequelize.define('Cliente_Jorge',{
+		id_cliente: {
+			field: 'id_cliente',
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},		
+		nome: {
+			field: 'nome',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		email: {
+			field: 'email',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		senha: {
+			field: 'senha',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		tipo_automovel: {
+			field: 'tipo_automovel',
+			type: DataTypes.INTEGER,
+			allowNull: false
+		},
+		descricao_mudanca: {
+			field: 'descricao_mudanca',
+			type: DataTypes.STRING,
+			allowNull: false
+		}
+	}, 
+	{
+		tableName: 'cliente_Jorge', 
+		freezeTableName: true, 
+		underscored: true,
+		timestamps: false,
+	});
+
+    return cliente_Jorge;
+};
---
+++ API-projeto-site-Jorge/public/script.js
@@ -0,0 +1,20 @@
+
+function abrir_hamburguer() {
+    document.getElementById("menu").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change");
+}
+
+// function enviar() {
+//     nome = String(inp_nome.value);
+//     email = String(inp_email.value);
+//     telefone = Number(inp_telefone.value);
+//     veiculos = Number(combo_veiculos.value);
+
+//     email.indexOf('@') == -1 || email.indexOf('.com') == -1 ? 
+//         alert('Email invalido') 
+//         : veiculos < 1 ?
+//             alert('Veiculo ainda não foi selecionado')
+//         : alert('Email cadastrado com sucesso');
+
+// }
