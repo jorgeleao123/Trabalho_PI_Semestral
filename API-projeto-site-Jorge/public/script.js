96101ea769cef2115aea85d8ba076befe9559192
Jorge Ricardo Leão <jotalion.4.jlo@gmail.com>
2021-05-23 21:34:20 -0300

    adicionando nova versão de login e página de comunidade

--- API-projeto-site-Jorge/public/script.js
+++ API-projeto-site-Jorge/public/script.js
@@ -5,6 +5,33 @@ function abrir_hamburguer() {
     document.getElementById("menu-bg").classList.toggle("change");
 }
 
+
+var cont_curt = 0;
+
+function curtir() {
+    ++cont_curt;
+    cont_curt % 2 == 0 ?
+        curtidas.innerHTML = '0'
+        : (curtidas.innerHTML = '1', descurtidas.innerHTML = '0');
+}
+
+function descurtir() {
+    ++cont_curt;
+    cont_curt % 2 == 0 ?
+        descurtidas.innerHTML = '0'
+        : (descurtidas.innerHTML = '1', curtidas.innerHTML = '0');
+}
+
+function comentar() {
+    var comentario = String(inp_comentario.value);
+    div_coment.innerHTML += `<span style="background-color: white;  
+                            color: black; 
+                            border-radius: 8px; 
+                            text-align: center; 
+                            padding: 8px;"> ${comentario} </span> <br> <br>`;
+}
+
+
 // function enviar() {
 //     nome = String(inp_nome.value);
 //     email = String(inp_email.value);
